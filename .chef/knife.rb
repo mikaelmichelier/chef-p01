ba7b8b194664689189dc20a5511727dd2a16ff90
MikaelMichelier <mikael.michelier@viastak.com>
2017-08-03 11:55:03 +0000

    Add Pem Keys

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -3,7 +3,7 @@
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "mikael"
-client_key               "#{current_dir}/mikael.pem"
-chef_server_url          "https://chef-srv.viastak.local/organizations/viastak"
+node_name                "administrator"
+client_key               "#{current_dir}/administrator.pem"
+chef_server_url          "https://chef-srv01/organizations/viastak-dev"
 cookbook_path            ["#{current_dir}/../cookbooks"]
--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -13,3 +13,4 @@ service 'apache2' do
 	action [:start, :enable]
 end
 
+
